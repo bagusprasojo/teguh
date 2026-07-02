0da007b96bf79e18cc8099911c45b36164103277
Bagus Laptop Baru <bagus@laptopbaru.com>
2026-07-02 20:54:04 +0700

    aktifkan django admin

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("django-admin/", admin.site.urls),
+    path("bagusprasojo/", admin.site.urls),
     path("ckeditor5/", include("django_ckeditor_5.urls")),
     path("", include("learning.urls")),
 ]
@@ -13,5 +13,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
